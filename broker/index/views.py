a54ee2fe51240f7b5eb6253140b1d184c28a1e22
analistaTr4u <analista.sistemas@trade4u.co>
2019-01-04 17:42:05 -0500

    payeer last

--- broker/index/views.py
+++ broker/index/views.py
@@ -11,13 +11,12 @@ m_amount = "150.00"
 m_curr = "USD"
 description = "Test payment"
 m_desc = binascii.b2a_base64(description.encode('utf8'))[:-1]
+m_desc = str(m_desc)[2:-1]
 m_key = "123"
 list_of_value_for_sign = map(str, [m_shop, m_orderid, m_amount, m_curr, m_desc, m_key])
 result_string = ":".join(list_of_value_for_sign)
-sign_hash = sha256(result_string)
+sign_hash = sha256(result_string.encode())
 m_sign = sign_hash.hexdigest().upper()
-
-
 
 contexto = {"m_shop": m_shop,
             "m_orderid": m_orderid,
